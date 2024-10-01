863da22fd629b04050add51992557fa866176324
Jan Voges <janvoges.git@icloud.com>
2024-10-01 19:26:59 +0200

    Move ensembl_id_to_gene_name() to utils.py

--- transmet/utils.py
+++ transmet/utils.py
@@ -112,3 +112,37 @@ def extract_tar(tar_path: str, extract_dir: str) -> None:
         print(f"Extraction completed: {extract_dir}")
     except f.TarError as e:
         raise ValueError(f"Error extracting TAR file: {e}")
+
+
+def ensembl_id_to_gene_name(ensembl_id: str) -> str:
+    """
+    Get the gene name for an Ensembl ID using the Ensembl REST API.
+
+    To map Ensembl stable IDs (such as gene, transcript, or protein IDs) to gene names
+    (i.e., HGNC symbols from the [HUGO Gene Nomenclature Committee](https://www.genenames.org)),
+    we use Ensembl's REST API.
+
+    Args:
+        ensembl_id: The Ensembl ID.
+
+    Returns:
+        The gene name.
+
+    Raises:
+        requests.exceptions.Request: If the request to the Ensembl REST API fails.
+    """
+    # The URL for the Ensembl REST API
+    url = (
+        f"https://rest.ensembl.org/lookup/id/{ensembl_id}?content-type=application/json"
+    )
+
+    try:
+        # Send a GET request to the Ensembl REST API
+        response = requests.get(url=url)
+        response.raise_for_status()
+
+        # Return the gene name from the JSON response
+        data = response.json()
+        return data.get("display_name", "No gene name found")
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"Failed to get gene name from Ensembl REST API: {e}")
