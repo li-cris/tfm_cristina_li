1ed313778dd6b2d2a592e65f93e98a09fc5b19fa
Jan Voges <janvoges.git@icloud.com>
2024-11-20 14:00:00 +0000

    Reorganize

--- src/transmet/filesystem.py
+++ src/transmet/filesystem.py
@@ -1,14 +1,13 @@
-"""Utility functions."""
+"""Filesystem utilities."""
 
 import subprocess
-from typing import Optional
 
 
-def get_git_root() -> Optional[str]:
+def get_git_root() -> str:
     """Return the root directory of the current Git repository.
 
     Returns:
-        The root directory of the current Git repository, or None if the command fails.
+        The root directory of the current Git repository, or "" if the command fails.
     """
     try:
         return subprocess.check_output(
@@ -20,4 +19,4 @@ def get_git_root() -> Optional[str]:
         print(f"Failed to get Git root: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-    return None
+    return ""
