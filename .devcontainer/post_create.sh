aafe35819c9b693cd2d99b3b6665e264e7855ab3
Jan Voges <janvoges.git@icloud.com>
2024-11-20 13:51:21 +0000

    Update devcontainer

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,6 +6,12 @@ sed --in-place '/"\[python\]": {/,/}/d' /home/vscode/.vscode-server/data/Machine
 # Install additional packages.
 sudo apt-get update && sudo apt-get install --yes shellcheck
 
+# Clone external repositories.
+git clone git@github.com:voges/compass-docker.git external/compass-docker
+git clone git@github.com:voges/gears-fork.git external/gears-fork
+git clone git@github.com:GuilleDufortFing/metavis.git external/metavis
+git clone git@github.com:voges/pertdata.git external/pertdata
+
 # Install metavis dependencies.
 sudo apt-get update && sudo apt-get install --yes graphviz libgraphviz-dev
 (cd "external/metavis" && pip3 --disable-pip-version-check install --requirement requirements.txt)
