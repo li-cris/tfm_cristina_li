3e15adcb57292145b904bf6753dfa61e9dee2e7f
Jan Voges <janvoges.git@icloud.com>
2025-02-19 08:43:06 +0100

    Rename Y_train to Y in data.py

--- src/lgem/data.py
+++ src/lgem/data.py
@@ -19,7 +19,7 @@ def load_data(
         dataset_name: Name of the dataset to load.
 
     Returns:
-        Y_train: Data matrix with shape (n_genes, n_perturbations).
+        Y: Data matrix with shape (n_genes, n_perturbations).
         perturbations: List of perturbations.
         genes: List of genes.
     """
@@ -30,10 +30,10 @@ def load_data(
     # Load the data from disk if they exist.
     artifacts_dir_path = os.path.join(get_git_root(), "artifacts", "lgem", dataset_name)
     if os.path.exists(artifacts_dir_path):
-        Y_train = torch.load(os.path.join(artifacts_dir_path, "Y_train.pt"))  # noqa: N806
+        Y = torch.load(os.path.join(artifacts_dir_path, "Y.pt"))  # noqa: N806
         perturbations = torch.load(os.path.join(artifacts_dir_path, "perturbations.pt"))
         genes = torch.load(os.path.join(artifacts_dir_path, "genes.pt"))
-        return Y_train, perturbations, genes
+        return Y, perturbations, genes
 
     # Load the dataset.
     ds = pertdata.PertDataset(
@@ -55,27 +55,27 @@ def load_data(
     # Pseudobulk the data per perturbation.
     n_perturbations = len(perturbations)
     n_genes = len(genes)
-    Y_train = np.zeros((n_genes, n_perturbations))  # noqa: N806
+    Y = np.zeros((n_genes, n_perturbations))  # noqa: N806
     for i, pert in tqdm(
         enumerate(perturbations),
         desc="Pseudobulking",
         total=n_perturbations,
         unit="perturbation",
     ):
-        Y_train[:, i] = adata[adata.obs["perturbation"] == pert].X.mean(axis=0)
-    Y_train = torch.from_numpy(Y_train).float()  # noqa: N806
+        Y[:, i] = adata[adata.obs["perturbation"] == pert].X.mean(axis=0)
+    Y = torch.from_numpy(Y).float()  # noqa: N806
 
     # Save the data to disk.
     os.makedirs(artifacts_dir_path)
-    torch.save(Y_train, os.path.join(artifacts_dir_path, "Y_train.pt"))
+    torch.save(Y, os.path.join(artifacts_dir_path, "Y.pt"))
     torch.save(perturbations, os.path.join(artifacts_dir_path, "perturbations.pt"))
     torch.save(genes, os.path.join(artifacts_dir_path, "genes.pt"))
 
-    return Y_train, perturbations, genes
+    return Y, perturbations, genes
 
 
 def compute_embeddings(
-    Y_train: torch.Tensor,  # noqa: N803
+    Y: torch.Tensor,  # noqa: N803
     perturbations: List[str],
     genes: List[str],
     d_embed: int = 10,
@@ -83,7 +83,7 @@ def compute_embeddings(
     """Compute gene and perturbation embeddings.
 
     Args:
-        Y_train: Data matrix with shape (n_genes, n_perturbations).
+        Y: Data matrix with shape (n_genes, n_perturbations).
         perturbations: List of perturbations.
         genes: List of genes.
         d_embed: Embedding dimension.
@@ -93,16 +93,16 @@ def compute_embeddings(
         P: Perturbation embedding matrix with shape (n_perturbations, d_embed).
         b: Bias vector with shape (n_genes).
     """
-    # Perform a PCA on Y_train to obtain the top d_embed principal components, which
-    # will serve as the gene embeddings G.
+    # Perform a PCA on Y to obtain the top d_embed principal components, which will
+    # serve as the gene embeddings G.
     pca = PCA(n_components=d_embed)
-    G = pca.fit_transform(Y_train)  # noqa: N806
+    G = pca.fit_transform(Y)  # noqa: N806
 
     # Extract perturbation embeddings P from G by subsetting G to only those rows
-    # corresponding to genes that have been perturbed in the training data.
+    # corresponding to genes that have been perturbed in the data.
     P = G[np.where(np.isin(genes, perturbations))[0], :]  # noqa: N806
 
     # Compute b as the average expression of each gene across all perturbations.
-    b = Y_train.mean(axis=1)
+    b = Y.mean(axis=1)
 
     return torch.from_numpy(G).float(), torch.from_numpy(P).float(), b
