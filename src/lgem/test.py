c9de6aa9e9781e91b8c19b0b6f511fe26dbd4a16
Jan Voges <janvoges.git@icloud.com>
2025-02-19 08:53:14 +0100

    Refactor training and testing functions to include device handling and improve loss calculation

--- src/lgem/test.py
+++ src/lgem/test.py
@@ -1,18 +1,17 @@
 import torch
 
 
-def test(model, criterion, test_dataloader):
+def test(model, criterion, test_dataloader, device):
+    model.to(device)
+
     model.eval()
-
     test_loss = 0.0
-    num_batches = 0
-
     with torch.no_grad():
         for batch_P, batch_Y in test_dataloader:  # noqa: N806
+            batch_P, batch_Y = batch_P.to(device), batch_Y.to(device)  # noqa: N806
             Y_predicted = model(batch_P)  # noqa: N806
             loss = criterion(Y_predicted, batch_Y.T)
             test_loss += loss.item()
-            num_batches += 1
+        avg_test_loss = test_loss / len(test_dataloader)
 
-    avg_test_loss = test_loss / num_batches
-    print(f"Average test loss: {avg_test_loss:.4f}")
+    return avg_test_loss
--- src/lgem/train.py
+++ src/lgem/train.py
@@ -1,22 +1,31 @@
 from tqdm import tqdm
 
 
-def train(model, criterion, optimizer, train_dataloader, n_epochs):
-    model.train()
+def train(
+    model,
+    criterion,
+    optimizer,
+    train_dataloader,
+    n_epochs,
+    device,
+):
+    model.to(device)
 
     pbar = tqdm(range(n_epochs), desc="Training", unit="epoch")
-    for _ in pbar:
-        epoch_loss = 0
 
+    for _ in pbar:
+        model.train()
+        train_loss = 0.0
         for batch_P, batch_Y in train_dataloader:  # noqa: N806
+            batch_P, batch_Y = batch_P.to(device), batch_Y.to(device)  # noqa: N806
             optimizer.zero_grad()
             Y_predicted = model(batch_P)  # noqa: N806
             loss = criterion(Y_predicted, batch_Y.T)
             loss.backward()
             optimizer.step()
-            epoch_loss += loss.item()
+            train_loss += loss.item()
+        avg_train_loss = train_loss / len(train_dataloader)
 
-        avg_epoch_loss = epoch_loss / len(train_dataloader)
-        pbar.set_postfix_str(f"Loss: {avg_epoch_loss:.4f}")
+        pbar.set_postfix_str(f"Training Loss: {avg_train_loss:.4f}")
 
     return model
