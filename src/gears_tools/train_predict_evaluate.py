9eac11dd0f694543d166e1fe3d2669b6cf767aff
li-cris <cli.2@alumni.unav.es>
2025-07-21 14:14:38 +0200

    Added single perturbations evaluation to script and slight change in PertData code due to error

--- src/gears_tools/train_predict_evaluate.py
+++ src/gears_tools/train_predict_evaluate.py
@@ -11,17 +11,20 @@ from scipy.stats import pearsonr
 import random
 
 # GEARS imports
-from gears import GEARS, PertData
+# from gears import GEARS, PertData (Slight change in PertData)
+from gears import GEARS
 
 # Own imports
-from data_utils.metrics import MMDLoss, compute_kld
+from gears_tools.pertdata import PertData
+from gears_tools.inference import evaluate_double, evaluate_single
+
 
 DATA_DIR_PATH = "data"
 MODELS_DIR_PATH = "models"
 RESULTS_DIR_PATH = "results"
 
-PREDICT_SINGLE = False
-PREDICT_DOUBLE = True
+PREDICT_SINGLE = True
+PREDICT_DOUBLE = False
 
 # Set to True if training only has single perturbations (train + val) and double perturbations are on test.
 SINGLE_TRAIN_ONLY = True
@@ -34,15 +37,16 @@ def set_seeds(seed: int) -> None:
 
 def train(
     pert_data: PertData, dataset_name: str, model_savedir: str,
-    split: str, seed: int, hidden_size: int, device: str, epochs: int = 20
+    split: str, seed: int, hidden_size: int, decoder_hidden_size: int,
+    device: str, epochs: int = 20
 ) -> str:
     """Set up, train, and save GEARS model."""
     print("Training GEARS model.")
     gears_model = GEARS(pert_data=pert_data, device=device)
-    gears_model.model_initialize(hidden_size=hidden_size)
+    gears_model.model_initialize(hidden_size=hidden_size, decoder_hidden_size=decoder_hidden_size)
     gears_model.train(epochs=epochs)
     model_name = (
-        f"gears_{dataset_name}_split_{split}_seed_{str(seed)}_hidden_size_{str(hidden_size)}"
+        f"gears_{dataset_name}_split_{split}_seed_{str(seed)}_hidden_size_{str(hidden_size)}_decoder_hidden_size_{str(decoder_hidden_size)}"
     )
     gears_model.save_model(path=os.path.join(model_savedir, model_name))
     return model_name
@@ -118,118 +122,6 @@ def predict(pert_data: PertData,
                 print(f"{double},{expressions_str}", file=f)
 
 
-def evaluate_double(adata: AnnData, model_name: str, results_savedir: str,
-                    pool_size: int = 250, seed: int = 42, top_deg: int = 20) -> None:
-    """Evaluate the predicted GEPs of double perturbations."""
-    # Load predicted GEPs.
-    df = pd.read_csv(
-        filepath_or_buffer=os.path.join(results_savedir, f"{model_name}_double.csv")
-    )
-
-    # Make results file path.
-    results_file_path = os.path.join(
-        results_savedir, f"{model_name}_double_change_metrics.csv"
-    )
-
-    with open(file=results_file_path, mode="w") as f:
-        print(
-            f"double,mmd_true_vs_ctrl,mmd_true_vs_pred,mse_true_vs_ctrl,mse_true_vs_pred,kld_true_vs_ctrl,kld_true_vs_pred,pearsonTop{top_deg}_true_vs_ctrl,pearson_pval_true_vs_ctrl,pearsonTop{top_deg}_true_vs_pred,pearson_pval_true_vs_pred",
-            file=f,
-        )
-
-        for i, double in enumerate(df["double"]):
-            # Get the predicted GEP for the current double perturbation.
-            pred_geps = df.loc[df["double"] == double]
-            pred_geps = pred_geps.iloc[0, 1:].tolist()
-            pred_geps = np.array([pred_geps])
-
-            # Get all the true GEPs with the current double perturbation.
-            double = double.replace("_", "+")
-            print(f"Evaluating double {i + 1}/{len(df['double'])}: {double}")
-            true_geps = adata[adata.obs["condition"] == double]
-
-            # Limiting n
-            if true_geps.n_obs>pool_size:
-                n = pool_size
-                random_indices = np.random.choice(true_geps.n_obs, size=n, replace=False)
-                true_geps = true_geps[random_indices, :]  
-            else:
-                # If less than pool size, randomly sample from all true_geps to avoid error in MMD computation
-                n = true_geps.n_obs
-
-            set_seeds(seed)
-
-            # Obtaining random sample of ctrl GEP
-            all_ctrl_geps = adata[adata.obs["condition"] == "ctrl"]
-            random_indices = np.random.choice(
-                all_ctrl_geps.n_obs, size=n, replace=False
-            )
-            ctrl_geps = all_ctrl_geps[random_indices, :]
-            pred_geps = csr_matrix(np.tile(pred_geps, reps=(n, 1)))
-
-            # Another random ctrl_gep
-            random_indices_2 = np.random.choice(
-                all_ctrl_geps.n_obs, size=n, replace=False
-            )
-            ctrl_geps_2 = all_ctrl_geps[random_indices_2, :]
-
-            # Tensor conversion and differential expression
-            ctrl_geps_tensor = torch.tensor(ctrl_geps.X.toarray())
-            ctrl_ctrl_geps_tensor = torch.tensor(ctrl_geps_2.X.toarray()) - ctrl_geps_tensor
-            true_ctrl_geps_tensor = torch.tensor(true_geps.X.toarray()) - ctrl_geps_tensor
-            pred_ctrl_geps_tensor = torch.tensor(pred_geps.toarray()) - ctrl_geps_tensor
-
-            # MMD setup.
-            mmd_sigma = 200.0
-            kernel_num = 10
-            mmd_loss = MMDLoss(fix_sigma=mmd_sigma, kernel_num=kernel_num)
-
-            # Compute MMD 
-            mmd_true_vs_ctrl = mmd_loss.forward(
-                            source=ctrl_ctrl_geps_tensor, target=true_ctrl_geps_tensor
-                        )
-
-            mmd_true_vs_pred = mmd_loss.forward(
-                source=pred_ctrl_geps_tensor, target=true_ctrl_geps_tensor
-            )
-
-            # Compute MSE
-            mse_true_vs_ctrl = torch.mean(
-                (true_ctrl_geps_tensor - ctrl_ctrl_geps_tensor) ** 2
-            ).item()
-            mse_true_vs_pred = torch.mean(
-                (true_ctrl_geps_tensor - pred_ctrl_geps_tensor) ** 2
-            ).item()
-
-            # Compute KLD
-            kld_true_vs_ctrl = compute_kld(true_ctrl_geps_tensor, ctrl_ctrl_geps_tensor)
-            kld_true_vs_pred = compute_kld(true_ctrl_geps_tensor, pred_ctrl_geps_tensor)
-
-            # Compute Pearson for top DEG
-            true_deg = true_ctrl_geps_tensor.mean(dim=0).cpu().detach().numpy()
-            ctrl_ctrl_deg = ctrl_ctrl_geps_tensor.mean(dim=0).cpu().detach().numpy()
-            pred_deg = pred_ctrl_geps_tensor.mean(dim=0).cpu().detach().numpy()
-            topdeg_idx = np.argsort(abs(true_deg))[-top_deg:]
-
-            pearson_true_vs_ctrl = pearsonr(true_deg[topdeg_idx], ctrl_ctrl_deg[topdeg_idx])
-            pearson_true_vs_pred = pearsonr(true_deg[topdeg_idx], pred_deg[topdeg_idx])
-
-
-            print(f"MMD (true vs. control):   {mmd_true_vs_ctrl:10.6f}")
-            print(f"MMD (true vs. predicted): {mmd_true_vs_pred:10.6f}")
-            print(f"MSE (true vs. control):   {mse_true_vs_ctrl:10.6f}")
-            print(f"MSE (true vs. predicted): {mse_true_vs_pred:10.6f}")
-            print(f"KLD (true vs. control):   {kld_true_vs_ctrl:10.6f}")
-            print(f"KLD (true vs. predicted): {kld_true_vs_pred:10.6f}")
-            print(f"Pearson Top {top_deg} DEG (true vs. control): {pearson_true_vs_ctrl.statistic:.6f} | p-value: {pearson_true_vs_ctrl.pvalue:.6f}")
-            print(f"Pearson Top {top_deg} DEG (true vs. predicted): {pearson_true_vs_pred.statistic:.6f} | p-value: {pearson_true_vs_pred.pvalue:.6f}")
-
-            print(
-                f"{double},{mmd_true_vs_ctrl},{mmd_true_vs_pred},{mse_true_vs_ctrl},{mse_true_vs_pred},{kld_true_vs_ctrl},{kld_true_vs_pred},{pearson_true_vs_ctrl.statistic},{pearson_true_vs_ctrl.pvalue},{pearson_true_vs_pred.statistic},{pearson_true_vs_pred.pvalue}",
-                file=f,
-            )
-
-
 def main():
     # Parse the command line arguments.
     parser = argparse.ArgumentParser(description="Train, predict, evaluate GEARS.")
@@ -239,6 +131,9 @@ def main():
     )
     parser.add_argument(
         "--hidden_size", type=int, default=64, help="Size of the hidden layers."
+    )
+    parser.add_argument(
+        "--decoder_hidden_size", type=int, default=16, help="Size of the hidden decoder layers."
     )
     parser.add_argument("--device", type=str, default="cuda", help="Device to use."
     )
@@ -291,7 +186,15 @@ def main():
         # See also: https://github.com/yhr91/GEARS_misc/blob/main/paper/Fig4_UMAP_train.py
         # This split of train test sizes keeps singles in training and validation and doubles in testing
         print("Preparing data split.")
-        if SINGLE_TRAIN_ONLY:
+
+        # Condition to evaluate only single perturbation dataset
+        if args.split == 'simulation_single':
+            print("Training and evaluating with single perturbation data.")
+            pertdata.adata = pertdata.adata[pertdata.adata.obs['condition'].str.contains('ctrl')]
+            pertdata.prepare_split(split=args.split, seed=current_seed)
+
+        # Condition to evaluate double perturbation dataset with single perturbations in training
+        elif SINGLE_TRAIN_ONLY and args.split != 'simulation_single':
             # If training only has single perturbations, we need to set the train_gene_set_size
             # to 1.0 and combo_seen2_train_frac to 0.0.
             print("Keeping only single perturbation samples in training.")
@@ -301,7 +204,10 @@ def main():
                 train_gene_set_size=1.0,
                 combo_seen2_train_frac=0.0
             )
+
+        # Condition to evaluate double perturbation dataset with both types in training
         else:
+            print("Training with both single and double perturbation data and evaluating with double perturbations.")
             pertdata.prepare_split(split=args.split, seed=current_seed)
         pertdata.get_dataloader(batch_size=32)
 
@@ -313,6 +219,7 @@ def main():
             split=args.split,
             seed=current_seed,
             hidden_size=args.hidden_size,
+            decoder_hidden_size=args.decoder_hidden_size,
             device=args.device,
             epochs=args.epochs
         )
@@ -324,10 +231,14 @@ def main():
         # Evaluate.
         pertdata = PertData(DATA_DIR_PATH)
         pertdata.load(data_path=os.path.join(DATA_DIR_PATH, data_name))
-        evaluate_double(adata=pertdata.adata, model_name=model_name,
-                        results_savedir=results_savedir, pool_size=args.pool_size,
-                        seed=current_seed, top_deg=args.top_deg)
-
+        if PREDICT_DOUBLE:
+            evaluate_double(adata=pertdata.adata, model_name=model_name,
+                            results_savedir=results_savedir, pool_size=args.pool_size,
+                            seed=current_seed, top_deg=args.top_deg)
+        if PREDICT_SINGLE:
+            evaluate_single(adata=pertdata.adata, model_name=model_name,
+                            results_savedir=results_savedir, pool_size=args.pool_size,
+                            seed=current_seed, top_deg=args.top_deg)
 
 if __name__ == "__main__":
     main()
